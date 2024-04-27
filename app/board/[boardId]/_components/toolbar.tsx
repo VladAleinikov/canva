de338b075d658985d4e8046a6a506510711436ce
Vladik <vlad.a.aleynikov@gmail.com>
2024-04-27 19:06:44 +0300

    add toolbar for borad page

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,16 +1,61 @@
+import { Circle, MousePointer2, Pencil, Redo2, Square, StickyNote, Type, Undo2 } from "lucide-react"
+import { ToolButton } from "./tool-button"
 
 
 export const Toolbar = () => {
       return (
             <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
                   <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-                        <div>Tool</div>
-                        <div>Tool</div>
-                        <div>Tool</div>
+                        <ToolButton
+                              label="Выбрать"
+                              icon={MousePointer2}
+                              onClick={() => { }}
+                              isActive={false}
+                        />
+                        <ToolButton
+                              label="Текст"
+                              icon={Type}
+                              onClick={() => { }}
+                              isActive={false}
+                        />
+                        <ToolButton
+                              label="Стикер"
+                              icon={StickyNote}
+                              onClick={() => { }}
+                              isActive={false}
+                        />
+                        <ToolButton
+                              label="Прямоугольник"
+                              icon={Square}
+                              onClick={() => { }}
+                              isActive={false}
+                        />
+                        <ToolButton
+                              label="Элипс"
+                              icon={Circle}
+                              onClick={() => { }}
+                              isActive={false}
+                        />
+                        <ToolButton
+                              label="Карандаш"
+                              icon={Pencil}
+                              onClick={() => { }}
+                              isActive={false}
+                        />
                   </div>
                   <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-                        <div>Tool</div>
-                        <div>Tool</div>
+                        <ToolButton
+                              label="Назад"
+                              icon={Undo2}
+                              onClick={() => { }}
+                              isActive={false}
+                        />
+                        <ToolButton
+                              label="Вперед"
+                              icon={Redo2}
+                              onClick={() => { }}
+                              isActive={false}
+                        />
                   </div>
             </div>
       )
