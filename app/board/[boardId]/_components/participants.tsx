cb7a24a0e2b0b30853d545163b2936342543a337
Vladik <vlad.a.aleynikov@gmail.com>
2024-04-27 18:53:51 +0300

    add users to board page

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,44 @@
+"use client"
 
+import { useOthers, useSelf } from "@/liveblocks.config"
+import { UserAvatar } from "./user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
+
+const MAX_SHOWN_OTHER_USERS = 3;
 
 export const Participants = () => {
+  const currentUser = useSelf();
+  const users = useOthers();
+  const hasMoreUsers = users.length > MAX_SHOWN_OTHER_USERS;
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      Participants
+      <div className="flex gap-x-2">
+        {users
+          .slice(0, MAX_SHOWN_OTHER_USERS)
+          .map(({ connectionId, info }) => (
+            <UserAvatar
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || "-"}
+              borderColor={connectionIdToColor(connectionId)}
+            />
+          ))
+        }
+        <UserAvatar
+          src={currentUser.info?.picture}
+          name={`${currentUser.info?.name} (ВЫ)`}
+          fallback={currentUser.info?.name?.[0] || "-"}
+          borderColor={connectionIdToColor(currentUser.connectionId)}
+        />
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`Ещё ${users.length - MAX_SHOWN_OTHER_USERS}`}
+            fallback={`+${users.length - MAX_SHOWN_OTHER_USERS}`}
+          />
+        )}
+      </div>
     </div>
   )
 }
