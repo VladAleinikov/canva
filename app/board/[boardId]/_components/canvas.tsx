cef6c66f5c90a1ac74c67983b7b5e31d6dd74599
Vladik <vlad.a.aleynikov@gmail.com>
2024-04-28 18:26:13 +0300

    add resizing for layer

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,13 +4,14 @@ import { useCallback, useMemo, useState } from "react"
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
-import { Camera, CanvasMode, CanvasState, Color, LayerType, Point } from "@/types/canvas"
+import { Camera, CanvasMode, CanvasState, Color, LayerType, Point, Side, XYWH } from "@/types/canvas"
 import { useCanRedo, useCanUndo, useHistory, useMutation, useOthersMapped, useStorage } from "@/liveblocks.config"
-import { connectionIdToColor, pointerEventToCanvasPoint } from "@/lib/utils"
+import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/lib/utils"
 import { CursorsPresence } from "./cursors-presence"
 import { nanoid } from 'nanoid'
 import { LiveObject } from "@liveblocks/client"
 import { LayerPreview } from "./layer-preview"
+import { SelectionBox } from "./selection-box"
 
 interface CanvasProps {
       boardId: string
@@ -64,6 +65,37 @@ export const Canvas = ({ boardId }: CanvasProps) => {
             setCanvasState({ mode: CanvasMode.None })
       }, [lastUsedColor])
 
+      const resizeSelectedLayer = useMutation((
+            { storage, self },
+            point: Point
+      ) => {
+            if (canvasState.mode !== CanvasMode.Resizing) {
+                  return
+            }
+
+            const bounds = resizeBounds(canvasState.initialBounds, canvasState.corner, point);
+
+            const liveLayers = storage.get("layers");
+            const layer = liveLayers.get(self.presence.selection[0]);
+
+            if (layer) {
+                  layer.update(bounds);
+            }
+       },[canvasState])
+
+      const onResizeHandlePointerDown = useCallback((
+            corner: Side,
+            initialBounds: XYWH
+      ) => {
+            history.pause();
+
+            setCanvasState({
+                  mode: CanvasMode.Resizing,
+                  initialBounds,
+                  corner
+            })
+      }, [history])
+
       const onWheel = useCallback((e: React.WheelEvent) => {
             setCamera((camera) => ({
                   x: camera.x - e.deltaX,
@@ -79,11 +111,17 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
             const current = pointerEventToCanvasPoint(e, camera);
 
+            if (canvasState.mode === CanvasMode.Resizing) {
+                  resizeSelectedLayer(current)
+            }
+
             setMyPresence({ cursor: current })
-      }, [])
+      }, [canvasState, resizeSelectedLayer, camera])
+
       const onPointerLeave = useMutation(({ setMyPresence }) => {
             setMyPresence({ cursor: null })
       }, [])
+      
       const onPointerUp = useMutation((
             { },
             e
@@ -183,6 +221,9 @@ export const Canvas = ({ boardId }: CanvasProps) => {
                                           selectionColor={layerIdsToColorSelection[layerId]}
                                     />
                               ))}
+                              <SelectionBox
+                                    onResizeHandlePointerDown={onResizeHandlePointerDown}
+                              />
                               <CursorsPresence />
                         </g>
                   </svg>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { Camera, Color } from "@/types/canvas";
+import { Camera, Color, Point, Side, XYWH } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -14,7 +14,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function connectionIdToColor(connectionId: number): string{
+export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
 }
 
@@ -30,4 +30,32 @@ export function pointerEventToCanvasPoint(
 
 export function colorToCss(color: Color) {
   return `rgb(${color.r}, ${color.g}, ${color.b})`
+}
+
+export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
+  const result = {
+    x: bounds.x,
+    y: bounds.y,
+    width: bounds.width,
+    height: bounds.height,
+  }
+
+  if ((corner & Side.Left) === Side.Left) {
+    result.x = Math.min(point.x, bounds.x + bounds.width);
+    result.width = Math.abs(bounds.x + bounds.width - point.x);
+  }
+  if ((corner & Side.Right) === Side.Right) {
+    result.x = Math.min(point.x, bounds.x);
+    result.width = Math.abs(point.x - bounds.x);
+  }
+  if ((corner & Side.Top) === Side.Top) {
+    result.y = Math.min(point.y, bounds.y + bounds.height);
+    result.height = Math.abs(bounds.y + bounds.height - point.y);
+  }
+  if ((corner & Side.Bottom) === Side.Bottom) {
+    result.y = Math.min(point.y, bounds.y);
+    result.height = Math.abs(point.y - bounds.y);
+  }
+
+  return result;
 }
